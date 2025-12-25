cc9f3853779edd49bddae67cabcb88bdc4d0f568
Ilyass <ioublale@gmail.com>
2025-12-25 17:56:33 +0100

    fix error

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,35 +1,34 @@
-import '../css/app.css';
+import '../css/app.css'
 
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import type { DefineComponent } from 'vue';
-import { createApp, h } from 'vue';
-import { initializeTheme } from './composables/useAppearance';
-import { ZiggyVue } from 'ziggy-js';
-import { Ziggy } from './ziggy';
+import { createInertiaApp } from '@inertiajs/vue3'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+import { createApp, h } from 'vue'
+import type { DefineComponent } from 'vue'
+import { initializeTheme } from './composables/useAppearance'
+import { ZiggyVue } from 'ziggy-js'
 
-
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
 
 createInertiaApp({
-    title: (title) => (title ? `${title} - ${appName}` : appName),
+  title: (title) => (title ? `${title} - ${appName}` : appName),
 
-    resolve: (name) =>
-        resolvePageComponent(
-            `./pages/${name}.vue`,
-            import.meta.glob<DefineComponent>('./pages/**/*.vue'),
-        ),
+  resolve: (name) =>
+    resolvePageComponent(
+      `./pages/${name}.vue`,
+      import.meta.glob<DefineComponent>('./pages/**/*.vue'),
+    ),
 
-    setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(ZiggyVue, Ziggy)
-            .mount(el);
-    },
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      // ✅ THIS IS THE FIX
+      .use(ZiggyVue, props.initialPage.props.ziggy)
+      .mount(el)
+  },
 
-    progress: {
-        color: '#4B5563',
-    },
-});
+  progress: {
+    color: '#4B5563',
+  },
+})
 
-initializeTheme();
+initializeTheme()
